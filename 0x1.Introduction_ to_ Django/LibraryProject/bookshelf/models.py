507920120cd75d5bc531c5f5a5154ca6db88bc1d
Andrewgh1 <andrewlaryea0001@gmail.com>
2024-08-11 20:37:48 +0000

    update to crud ops

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
     def __str__(self):
-        return self.title , self.author, self.publication_year
+        return self.title 
 
 class Arthur(models.Model):
     name = models.CharField(max_length=100)
