f9c3e8846e5ece0873c866ee533d281f5f7eee0e
Andrewgh1 <andrewlaryea0001@gmail.com>
2024-08-17 12:11:25 +0000

    Added user authentication features to relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, UserLoginView, UserLogoutView, UserRegisterView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', UserRegisterView.as_view(), name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.views.generic import DetailView #cbv
+from .models import Book, Library #fbv,cbv
+from django.contrib.auth.views import LoginView ,LogoutView #setup user login and logout authentication views
+from django.contrib.auth.forms import UserCreationForm #setup user login and logout authentication views
+from django.urls import reverse_lazy #setup user login and logout authentication views
+from django.views.generic.edit import CreateView #setup user login and logout authentication views
+
 
 # Create your views here.
 def home(request):
@@ -17,3 +22,19 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+#setup user login and logout authentication views
+#LoginView
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+
+#LogoutView
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+#RegisterationView
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
