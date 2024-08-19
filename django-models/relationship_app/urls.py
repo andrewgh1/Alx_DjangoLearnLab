9b53a243ee4224964490f5fb48500d28fdf75f3a
Andrewgh1 <andrewlaryea0001@gmail.com>
2024-08-19 11:55:54 +0000

    update ,relationship_app

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import home_view, admin_view, librarian_view, member_view
+from .views import list_books
 
 
 
@@ -13,7 +14,7 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
-    path('books/', views.list_books, name='list_books'),
+    path('books/',list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     #path('login/', views.login_view, name='login'),
     #path('logout/', views.logout_view, name='logout'),
