3a56c07bdb37c0ea0fac3ee9cef6999d95c58346
Andrewgh1 <andrewlaryea0001@gmail.com>
2024-08-19 00:34:51 +0000

    update ,relationship_app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -16,7 +16,7 @@ def index(request):
 #fbv
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 #cbv
 class LibraryDetailView(DetailView):
