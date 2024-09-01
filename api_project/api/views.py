d44c7a03344d112f86adda6d4410bbab294b3ff6
Andrewgh1 <andrewlaryea0001@gmail.com>
2024-09-01 22:59:42 +0000

    Django REST Framework Authentication and Permissions Implementation

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics 
+from rest_framework import generics,viewsets
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from .models import Book 
 from .serializers import BookSerializer
 
@@ -13,3 +14,10 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+    
