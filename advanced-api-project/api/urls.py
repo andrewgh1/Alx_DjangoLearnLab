16bfa42c289aca5e321757ebc2112d91f974f9dc
Andrewgh1 <andrewlaryea0001@gmail.com>
2024-09-08 23:26:31 +0000

    Custom Serializers

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,8 +1,12 @@
+# urls.py
+
 from django.urls import path
-from .views import BookListView, BookDetailView, BookCreateView
+from .views import BookListView, BookCreateView, BookDetailView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
