e5ab13292f947377498e6423b7301bf715a83a64
Andrewgh1 <andrewlaryea0001@gmail.com>
2024-09-15 21:05:29 +0000

    Post Management features

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Profile, Post
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'location', 'birth_date']
+
+class CreatePostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
+class UpdatePostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'})
+
+        }
