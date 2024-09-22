e6c3c1c6ef87ea69e57fdb34c863dbdc768f121c
Andrewgh1 <andrewlaryea0001@gmail.com>
2024-09-22 23:43:50 +0000

    Notification and Likes Functionality

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -89,11 +89,16 @@ WSGI_APPLICATION = "social_media_api.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sm_db',
+        'USER': 'root',
+        'PASSWORD': 'Andrew98L',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
